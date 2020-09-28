3d53bfd83c3c292aea8d3ff4e0aaa6edf384064c
Subu Kaundinya <subu.kaundinya@gmail.com>
2020-09-28 11:51:32 -0400

    I made my first escape room

---
+++ script.js
@@ -0,0 +1,8 @@
+function congratswin(arg1) {
+    document.getElementById("congratswin").innerHTML=arg1;
+    console.log(arg1)
+}
+function emailme(arg1) {
+    document.getElementById("emailme").innerHTML=arg1;
+    console.log(arg1)
+}
